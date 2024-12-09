43eb0bc8fed61c17ae20883d4df620447e8f0cd3
fhvvbvn <153986637+fhvvbvn@users.noreply.github.com>
2024-12-09 18:42:03 +0800

    Update mian.sh

--- script/mian.sh
+++ script/mian.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 # 下载规则
+curl -o i-1.txt https://filters.adtidy.org/ios/filters/122_optimized.txt
 curl -o i-1.txt https://filters.adtidy.org/ios/filters/2_optimized.txt
 
 # 合并规则并去除重复项
 cat i*.txt > i-mergd.txt
-cat i-mergd.txt | grep -v '^!' | grep -v '^！' | grep -v '^# ' | grep -v '##' | grep -v '#@#' | grep -v '^\【' > i-tmpp.txt
+cat i-mergd.txt | grep -v '^!' | grep -v '^！' | grep -v '^# ' | grep -v '^# ' | grep -v '^\[' | grep -v '^\【' > i-tmpp.txt
 sort -n i-tmpp.txt | uniq > i-tmp.txt
 
 python rule.py i-tmp.txt
@@ -20,11 +21,19 @@ echo "! Description: 该规则合并自jiekouAD，AdGuard中文语言规则，Ad
 echo "! Homepage: https://github.com/damengzhu/abpmerge" >> i-tpdate.txt
 echo "! Version: `TZ=UTC-8 date +"%Y-%m-%d %H:%M:%S"`" >> i-tpdate.txt
 echo "! Total count: $num" >> i-tpdate.txt
-cat i-tpdate.txt i-tmp.txt > Anti-CSS.txt
+cat i-tpdate.txt i-tmp.txt > abpmerge.txt
+
+cat "abpmerge.txt" | grep \
+-e "^#" \
+> "CSSRule.txt"
+cat "abpmerge.txt" | grep \
+-v "##" \
+-v "#@#" \
+> "Anti-CSSRule.txt"
+
 
 # 删除缓存
 rm i-*.txt
 
 #退出程序
 exit
-
